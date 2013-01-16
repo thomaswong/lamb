2611235c4034ccb42d414d31dfb0591fdd2100df
thomas wong <thom.sweet@aol.com>
2013-01-16 23:12:53 +0800

    add db

--- ws.py
+++ ws.py
@@ -2,11 +2,16 @@ import soaplib
 from soaplib.core.service import rpc, DefinitionBase, soap
 from soaplib.core.model.primitive import String, Integer
 from soaplib.core.server import wsgi
-from soaplib.core.model.clazz import Array
+from soaplib.core.model.clazz import Array, ClassModel
 import tornado.web
 import tornado.wsgi
 import tornado.httpserver
+from sqlalchemy import create_engine
+from sqlalchemy.schema import MetaData, Table
 
+
+
+        
 
 class HelloWorldService(DefinitionBase):
     @soap(String,Integer,_returns=Array(String))
@@ -23,9 +28,29 @@ class HelloWorldService(DefinitionBase):
             results.append('No Cache Anymore')
         return results
 
+class BackInBlack(DefinitionBase):
+    class Item(ClassModel):
+        # class namespace for soap target namespace
+        __namespace__ = "Item"
+        Material = String
+        PurchaseGroup = String
+
+    """docstring for BackInBlack"""
+    @soap(_returns=Array(Item))
+    def list_items(self):
+        engine = create_engine('sqlite:///test.sqlite')
+        meta = MetaData()
+        meta.bind = engine
+        table = Table("CT_MATERIAL", meta, autoload=True)
+        qry = table.select()
+        res = qry.execute()
+        
+        return [ (row['Material'], row['PurchaseGroup']) for row in res.fetchall() ]
+        
+
 if __name__=='__main__':
     try:
-        soap_application = soaplib.core.Application([HelloWorldService], 'tns')
+        soap_application = soaplib.core.Application([HelloWorldService, BackInBlack], 'tns')
         wsgi_application = wsgi.Application(soap_application)
         tornado_container =tornado.wsgi.WSGIContainer(wsgi_application)
         http_server = tornado.httpserver.HTTPServer(tornado_container)
